9494973f3c16ddb97f5a51401f18e611a94c2baa
Oscar Lindqvist <lios16@nti.umea>
2018-11-20 10:39:56 +0100

    flex

---
+++ oliver.py
@@ -0,0 +1,24 @@
+#hej = "monika"
+#hej = 1
+#ord(a)
+#chr(127)
+#int("1")
+#str(0)
+#hej = ord("a")
+#hej = chr(10)
+#print("värde")
+#print("hej")
+
+stringvar = "hejsan"
+intvar = 123
+
+print(stringvar + " " + str(intvar))
+
+letter = "hejsan"
+for l in letter:
+    print(l)
+
+i = 0
+while i < len(letter):
+    print(letter[i])
+    i = i + 1
